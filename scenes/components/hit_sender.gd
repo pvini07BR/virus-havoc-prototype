c2bf2ce887d472b49530a623f08a27591db14a75
pvini07BR <pvini07br@proton.me>
2025-06-21 21:23:48 -0300

    damage cooldown

--- scenes/components/hit_sender.gd
+++ scenes/components/hit_sender.gd
@@ -3,9 +3,11 @@ extends Area2D
 class_name HitSender
 
 @export var damage: float = 1.0
+@export var delete_parent_at_hit: bool = true
 
 func _ready() -> void:
 	pass
 
 func delete_itself():
-	get_parent().queue_free()
+	if delete_parent_at_hit:
+		get_parent().queue_free()
--- scenes/components/hitreceiver.gd
+++ scenes/components/hitreceiver.gd
@@ -2,10 +2,39 @@ extends Area2D
 
 class_name HitReceiver
 
+enum DamageMode {
+	ACCUMULATE,
+	COOLDOWN
+}
+
 @export var health: Health
+@export var damage_mode: DamageMode
+
+@onready var acc_timer: Timer = $AccumulateTimer
+@onready var cooldown_timer: Timer = $CooldownTimer
+
+var damage_buffer: float = 0.0
 
 func _on_area_entered(area: Area2D) -> void:
 	if health:
 		if area is HitSender:
-			health.damage(area.damage)
+			if damage_mode != DamageMode.COOLDOWN:
+				health.damage(area.damage)
+			
+			if cooldown_timer.is_stopped() and damage_mode == DamageMode.COOLDOWN:
+				health.damage(area.damage)
+				health.invincible = true
+				cooldown_timer.start()
+					
+			if acc_timer.is_stopped():
+				acc_timer.start()
+				
+			if damage_mode == DamageMode.ACCUMULATE:
+				damage_buffer += area.damage
 			area.delete_itself()
+
+func _on_acc_timer_timeout() -> void:
+	damage_buffer = 0.0
+	
+func _on_cooldown_timer_timeout() -> void:
+	health.invincible = false
