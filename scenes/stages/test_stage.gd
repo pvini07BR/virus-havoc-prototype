9941a8e22ff931df7499bf7f905574bc3ea24cf0
pvini07BR <pvini07br@proton.me>
2025-06-23 22:02:33 -0300

    bub fix

--- scenes/stages/test_stage.gd
+++ scenes/stages/test_stage.gd
@@ -4,6 +4,6 @@ var enemy = preload("res://scenes/viruses/PaperZipBomber.tscn")
 
 func _on_spawn_timer_timeout() -> void:
 	var inst = enemy.instantiate()
-	inst.position.x = 1200.0
+	inst.position.x = 1280.0
 	inst.position.y = 720.0 / 4.0
 	add_child(inst)
--- scenes/viruses/paper_zip_bomber.gd
+++ scenes/viruses/paper_zip_bomber.gd
@@ -5,11 +5,15 @@ extends CharacterBody2D
 func _process(_delta: float) -> void:
 	velocity.x = -400
 	move_and_slide()
+	
+	if self.position.x < 0.0:
+		queue_free()
 
 func _on_scanning_body_entered(body: Node2D) -> void:
 	if body == GameManager.get_player():
 		zip_bomb.top_level = true
-		zip_bomb.global_position.x = global_position.x
-		zip_bomb.global_position.y = global_position.y + 50
+		zip_bomb.global_position.x = global_position.x + 1
+		zip_bomb.global_position.y = global_position.y + 16
 		zip_bomb.velocity.y = 400
+		zip_bomb.z_index = self.z_index - 1
 		zip_bomb.disabled = false
